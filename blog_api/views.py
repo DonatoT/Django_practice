4e6c34148184dd3e60cd278656576761cd67c069
Donato TCHINGOUA <donatotchingoua@outlook.com>
2022-08-30 17:21:34 +0100

    set data model on the project

---
+++ blog_api/views.py
@@ -0,0 +1,14 @@
+from rest_framework import generics
+from blog.models import Post
+from .serializers import postSerializer
+# Create your views here.
+
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = postSerializer
+    pass 
+
+class PostDetail(generics.RetrieveDestroyAPIView):
+    pass
+
